1d56977a8666c5abd278639894170ce5ed2b240c
propstgonz <propstgonz@baronette.es>
2025-09-22 19:13:45 +0200

    Finalizada primera versión de la página "Cosas"

---
+++ src/data/cosas.ts
@@ -0,0 +1,54 @@
+import type { Cosa } from "../types/cosas";
+
+/*
+## PLANTILLA DEL MAPEO
+  {
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+  },
+*/
+
+export const cosas: Cosa[] = [
+    {
+    img: "https://propstgonz.baronette.es/_image?href=%2F_astro%2Fpropstfake.CzddGpRt.gif&w=292&h=290&f=webp",
+    title: "La página de Propst",
+    description: "Mi página personal/portfolio",
+    link: "https://propstgonz.baronette.es/",
+  },
+  {
+    img: "https://fasealterna.es/_astro/background-1.D_sJXt5u.webp",
+    title: "Fase Alterna",
+    description: "El grupo del puto Mezzenas (Págame basura)",
+    link: "https://fasealterna.es/",
+  },
+
+/*
+## Aquí irá la página graciosa que le voy a dedicar a Javier Tebas
+  {
+    img: "",
+    title: "",
+    description: "",
+    link: "",
+  },
+*/
+  {
+    img: "http://patinni.duckdns.org/favicon/favicon.png",
+    title: "PokéDle",
+    description: "Un juego de adivinar pokémons para gente de 300 kg by Patinni",
+    link: "http://patinni.duckdns.org/",
+  },
+  {
+    img: "/src/assets/img/trollface.png",
+    title: "Mi primera web",
+    description: "Tal cual, mi primera web",
+    link: "http://localhost:80",
+  },
+  {
+    img: "/src/assets/img/baronette.png",
+    title: "baronette.es (Primera versión)",
+    description: "La primera versión de Baronette.es",
+    link: "https://github.com/propstgonz/baronette-webapp-old",
+  },
+];
